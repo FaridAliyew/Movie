fa284f98a7e051737e129bcd796874368a3deb91
CAvid <c4vid2005@gmail.com>
2024-09-21 13:02:48 +0400

    footer

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,5 +1,7 @@
 import Carusel from "./components/Carusel";
+import Footer from "./components/Footer";
 import Header from "./components/Header";
+import LastestMovie from "./components/LastestMovie";
 import Navbar from "./components/Navbar";
 import Section from "./components/Section";
 import './style/style.css'
@@ -13,6 +15,8 @@ function App() {
       <Navbar/>
       <Carusel/>
       <Section/>
+      <LastestMovie/>
+      <Footer/>
     </div>
   );
 }
